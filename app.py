c95e0da0303c4ee12dcfcf635b46a7af97333836
Tanya Yushyna <tanyayushyna@192.168.1.104>
2025-11-29 00:20:33 +0300

    Min-min 002

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "IT WORKS!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/health')
+def health():
+    return {"status": "ok"}, 200
+
+# if __name__ == '__main__':
+#     # Для локальной разработки
+#     port = int(os.getenv("PORT", 5000))
+#     app.run(host='0.0.0.0', port=port)
