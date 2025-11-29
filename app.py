b5b0ca1e25e5522b406604e08fb5f66285f9f887
Tanya Yushyna <tanyayushyna@192.168.1.104>
2025-11-29 21:42:54 +0300

    Min-min 032

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def hello():
 def health_check():
     return "OK", 200
 
-if __name__ == "__main__":
-    # Запуск Flask на всех интерфейсах и на порту 8080
-    app.run(host='0.0.0.0', port=8080)
+# if __name__ == "__main__":
+#     # Запуск Flask на всех интерфейсах и на порту 8080
+#     app.run(host='0.0.0.0', port=8080)
