6bd28aeaedbd243983282d9632f64dc638205980
Tanya Yushyna <tanyayushyna@192.168.1.104>
2025-11-30 20:05:54 +0300

    Min-min 069

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ print("=" * 50, file=sys.stderr)
 
 @app.route('/')
 def hello():
-    return "Сайт работает! (Успех 068!)"
+    return "Сайт работает! (Успех 069!)"
 
 @app.route('/health')
 def health_check():
